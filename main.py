7351c05c7776609b5f9975706f4e54e3ce3eea38
Malins062 <6328673@gmail.com>
2022-03-28 12:05:10 +0300

    Add requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from cv2 import cv2 as cv
 
-
 if __name__ == '__main__':
     # print(dir(numpy))
     print(cv.__version__)
 
-    img = cv.imgread('img/pic1.jpg')
-    cv.imshow('Result',
-               img)
-    cv.waitkey(0)
-    cv.destroyallwindows()
+    img = cv.imread('img/pic1.jpg')
+    cv.imshow('Result', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
